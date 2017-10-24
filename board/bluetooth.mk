c367e79968560a37748adf485ded4e9eee147bfc
root <stenkinevgeniy@gmail.com>
2017-10-24 16:17:47 +0300

    1. Remove bluetooth paths - use configs from main device tree path 2. Try remove fast/slow charging (because our kernel send incompatible values)

--- board/bluetooth.mk
+++
@@ -1,8 +0,0 @@
-####################
-# Bluetooth        #
-####################
-
-BOARD_CUSTOM_BT_CONFIG := $(VENDOR_PATH)/bluetooth/libbt_vndcfg.txt
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_HAVE_SAMSUNG_BLUETOOTH := true
